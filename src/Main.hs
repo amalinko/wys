71433847815fb71935611774c958b6d59172d6b4
Artem <a.malinko@elama.ru>
2018-08-07 22:49:42 +0300

    add simple string parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,40 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
 main :: IO ()
-main = print "Hello"
+main = do
+  args <- getArgs
+  case args of
+    (h:_) -> putStrLn $ readExpression h
+    _ -> putStrLn "Should be one arg"
+
+readExpression :: String -> String
+readExpression input =
+  case parse (parseLString) "lisp" input of
+    Right val -> "Found value: " ++ show val
+    Left err -> "No match: " ++ show err
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal]
+               LispVal
+  | LNumber Int
+  | LString String
+  | LBool Bool
+  deriving (Eq, Show)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseLString :: Parser LispVal
+parseLString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ LString x
