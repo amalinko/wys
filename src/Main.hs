c4707d6d3930b57ec7edd976eea201df3d210c40
Artem <a.malinko@elama.ru>
2018-08-07 21:57:49 +0300

    first commit

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+main :: IO ()
+main = print "Hello"
